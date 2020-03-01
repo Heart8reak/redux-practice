fa63b70ff1e9be6ad863d497b0e8170c60ca57f4
Carlos Baez <carlosbaez@Green-Planet.attlocal.net>
2020-03-01 12:32:50 -0500

    created a simple counter with redux

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useSelector, useDispatch } from 'react-redux'
+import { increment, decrement } from './actions/index'
 
-function App() {
+
+function App(props) {
+  const count = useSelector(state => state)
+  const dispatch = useDispatch()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-header">
+      <h2>{count}</h2>
+      <span>
+        <button onClick={() => dispatch(decrement())}>-</button>
+        <button onClick={() => dispatch(increment())}>+</button>
+      </span>
     </div>
-  );
+  )
 }
 
-export default App;
+
+
+// function mapStateToProps(state) {
+
+//   return {
+//     count: state
+//   }
+// }
+
+// const mapDispatchToProps = {
+//   oranges: increment,
+//   apples: decrement
+// }
+
+// export default connect(mapStateToProps, mapDispatchToProps)(App)
+export default App
